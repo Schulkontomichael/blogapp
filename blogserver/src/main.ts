539366a6f5599df8fd4c83729cf5007bbf37cafe
Schnittger1986 <schnittger@microdemos.de>
2024-02-23 08:23:55 +0000

    final version

--- blogserver/src/main.ts
+++ blogserver/src/main.ts
@@ -4,34 +4,28 @@ import { FastifyAdapter } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 const CORS_OPTIONS = {
-  // origin: ['https://refactored-broccoli-pjrrvwppvvg379gq-4200.app.github.dev/'], // or '*' or whatever is required
-  origin: ['*'], // or '*' or whatever is required
-  allowedHeaders: [
-    'Access-Control-Allow-Origin',
-    'Origin',
-    'X-Requested-With',
-    'Accept',
-    'Content-Type',
-    'Authorization',
-  ],
-  credentials: true,
-  methods: ['GET', 'PUT', 'OPTIONS', 'POST', 'DELETE'],
-};
-
-async function bootstrap() {
-  const adapter = new FastifyAdapter();
-//  adapter.enableCors(CORS_OPTIONS);
-  
-const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule, adapter,
-  );
-  app.enableCors({
-    origin: '*',
+  //  origin: ['https://refactored-capybara-r4gg96pp9xwf9pg-4200.app.github.dev/api'], // or '*' or whatever is required
+    origin: ['*'], // or '*' or whatever is required
+    allowedHeaders: [
+      'Access-Control-Allow-Origin',
+      'Origin',
+      'X-Requested-With',
+      'Accept',
+      'Content-Type',
+      'Authorization',
+    ],
+    exposedHeaders: 'Authorization',
+    credentials: true,
     methods: ['GET', 'PUT', 'OPTIONS', 'POST', 'DELETE'],
-    allowedHeaders: 'Access-Control-Allow-Origin',
-    credentials:true,
-  });
-  app.setGlobalPrefix('/api');
-  await app.listen(3000);
-}
-bootstrap();
+  };
+  
+  async function bootstrap() {
+    const adapter = new FastifyAdapter();
+    adapter.enableCors(CORS_OPTIONS);
+    const app = await NestFactory.create<NestFastifyApplication>(
+      AppModule, adapter,
+    );
+    app.setGlobalPrefix('/api');
+    await app.listen(3000);
+  }
+  bootstrap();
